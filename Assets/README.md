cd83ba97d8a6a8c88541337f7ea90fd936b02c6f
pg-goose <pau.galopa.dev@gmail.com>
2025-11-12 12:11:09 +0100

    Initial clean commit (history squashed; Unity generated files removed)

---
+++ Assets/README.md
@@ -0,0 +1,22 @@
+* [ ] 3 pantalles: Menú, Jugar i Crèdits
+* [ ] Càmera ortogràfica orientada com al Bloxorz
+* [ ] Fons de cada nivell no sòlid
+* [x] Moviment/rotació del bloc amb WASD i fletxes
+* [ ] Si alguna part del bloc no està sustentada, cau i es repeteix el nivell
+* [ ] Si el bloc vertical entra al tile de destí, llisca dins i passa al següent nivell
+* [ ] Es pot tornar al menú principal des del joc
+* [ ] Espai per canviar el cub controlat quan està dividit
+
+* [ ] Botons rodons (per contacte): activen ponts
+* [ ] Botons en creu (només en vertical)
+* [ ] Tiles de divisió: separen el bloc en dos; si es toquen, es recombinen
+* [ ] Tiles taronja: no sostenen el bloc vertical sencer; els cubs separats sí que hi poden passar
+
+* [ ] 10 nivells de dificultat creixent (del Bloxorz original o propis)
+* [ ] Tecles 0–9 per saltar directament al nivell corresponent
+* [ ] Sortides: guanyar (girar i pujar) i perdre (caure) com al Bloxorz
+
+* [ ] HUD amb el total de moviments emprats per guanyar tots els nivells anteriors
+* [ ] So i música
+* [ ] Presentació del nivell amb tiles pujant des de baix
+
---
+++ Assets/Scripts/MoveCube4.cs
@@ -0,0 +1,131 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+// MoveCube manages cube movement. WASD + Cursor keys rotate the cube in the
+// selected direction. If the cube is not grounded (has a tile under it), it falls.
+// Some events trigger corresponding sounds.
+
+
+public class MoveCube : MonoBehaviour
+{
+    InputAction moveAction; 		// Input action to capture player movement (WASD + cursor keys)
+
+    bool bMoving = false; 			// Is the object in the middle of moving?
+	bool bFalling = false; 			// Is the object falling?
+    
+	public float rotSpeed; 			// Rotation speed in degrees per second
+    public float fallSpeed; 		// Fall speed in the Y direction
+
+    Vector3 rotPoint, rotAxis; 		// Rotation movement is performed around the line formed by rotPoint and rotAxis
+	float rotRemainder; 			// The angle that the cube still has to rotate before the current movement is completed
+    float rotDir; 					// Has rotRemainder to be applied in the positive or negative direction?
+    LayerMask layerMask; 			// LayerMask to detect raycast hits with ground tiles only
+
+    public AudioClip[] sounds; 		// Sounds to play when the cube rotates
+    public AudioClip fallSound; 	// Sound to play when the cube starts falling
+	
+	
+	// Determine if the cube is grounded by shooting a ray down from the cube location and 
+	// looking for hits with ground tiles
+
+    bool isGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.0f, layerMask))
+            return true;
+
+        return false;
+    }
+
+    // Start is called once after the MonoBehaviour is created
+    void Start()
+    {
+		// Find the move action by name. Done once in the Start method to avoid doing it every Update call.
+        moveAction = InputSystem.actions.FindAction("Move");
+		
+		// Create the layer mask for ground tiles. Done once in the Start method to avoid doing it every Update call.
+        layerMask = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bFalling)
+        {
+			// If we have fallen, we just move down
+            transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        }
+        else if (bMoving)
+        {
+			// If we are moving, we rotate around the line formed by rotPoint and rotAxis an angle depending on deltaTime
+			// If this angle is larger than the remainder, we stop the movement
+            float amount = rotSpeed * Time.deltaTime;
+            if(amount > rotRemainder)
+            {
+                transform.RotateAround(rotPoint, rotAxis, rotRemainder * rotDir);
+                bMoving = false;
+            }
+            else
+            {
+                transform.RotateAround(rotPoint, rotAxis, amount * rotDir);
+                rotRemainder -= amount;
+            }
+        }
+        else
+        {
+			// If we are not falling, nor moving, we check first if we should fall, then if we have to move
+            if (!isGrounded())
+            {
+                bFalling = true;
+				
+				// Play sound associated to falling
+                AudioSource.PlayClipAtPoint(fallSound, transform.position, 1.5f);
+            }
+			
+			// Read the move action for input
+            Vector2 dir = moveAction.ReadValue<Vector2>();
+            if(Math.Abs(dir.x) > 0.99 || Math.Abs(dir.y) > 0.99)
+            {
+				// If the absolute value of one of the axis is larger than 0.99, the player wants to move in a non diagonal direction
+                bMoving = true;
+				
+				// We play a random movemnt sound
+                int iSound = UnityEngine.Random.Range(0, sounds.Length);
+                AudioSource.PlayClipAtPoint(sounds[iSound], transform.position, 1.0f);
+				
+				// Set rotDir, rotRemainder, rotPoint, and rotAxis according to the movement the player wants to make
+                if (dir.x > 0.99)
+                {
+                    rotDir = -1.0f;
+                    rotRemainder = 90.0f;
+                    rotAxis = new Vector3(0.0f, 0.0f, 1.0f);
+                    rotPoint = transform.position + new Vector3(0.5f, -0.5f, 0.0f);
+                }
+                else if (dir.x < -0.99)
+                {
+                    rotDir = 1.0f;
+                    rotRemainder = 90.0f;
+                    rotAxis = new Vector3(0.0f, 0.0f, 1.0f);
+                    rotPoint = transform.position + new Vector3(-0.5f, -0.5f, 0.0f);
+                }
+                else if (dir.y > 0.99)
+                {
+                    rotDir = 1.0f;
+                    rotRemainder = 90.0f;
+                    rotAxis = new Vector3(1.0f, 0.0f, 0.0f);
+                    rotPoint = transform.position + new Vector3(0.0f, -0.5f, 0.5f);
+                }
+                else if (dir.y < -0.99)
+                {
+                    rotDir = -1.0f;
+                    rotRemainder = 90.0f;
+                    rotAxis = new Vector3(1.0f, 0.0f, 0.0f);
+                    rotPoint = transform.position + new Vector3(0.0f, -0.5f, -0.5f);
+                }
+            }
+        }
+    }
+
+}
